f4be8306ae7608b4ebcdf699a0ecb03e7338afe0
cy0707 <674986077@qq.com>
2017-02-17 19:27:18 +0800

    完整的demo的数字键盘，以及获取值，但是光标还是不能移动

--- keyboard/number.js
+++ keyboard/number.js
@@ -1,29 +1,48 @@
-var input = document.querySelector('.input');
-var numKeyboard = document.querySelector('.num-keyboard');
-var cancle = document.querySelector('#cancle');
-var cursor = document.querySelector('.cursor');
+window.onload = function(){
 
-// 数字键盘的弹起
-input.addEventListener('click', function(){
-	numKeyboard.style.display = 'block';
+	var input = document.querySelector('.input'); //输入的金额
+	var numKeyboard = document.querySelector('.num-keyboard'); //键盘
+	var cancle = document.querySelector('#cancle'); //删除
+	var cursor = document.querySelector('.cursor'); //光标
+	var submit = document.querySelectorAll('.sumbtn input'); //提交
+	var sum = document.querySelector('#sum'); //提交的金额
+
+	//进入页面，键盘弹起和光标出现
+	numKeyboard.style.bottom = 0;
 	cursor.style.display = 'inline-block';
 
-}, false);
-
-// 输入数字
-numKeyboard.addEventListener('click', function(e){
-	if (e.target == cancle) {
-		var inputStr = input.innerHTML;
-		console.log(inputStr.length);
-		input.innerHTML = inputStr.substring(0, inputStr.length-1);
-
-	}else{
-		if (e.target.className == 'num-item') {
-			var numVal = e.target.firstChild.nodeValue;
-         var span = document.createElement('span');
-         var spanNode = span.appendChild(document.createTextNode(numVal));
-			input.insertBefore(spanNode, input.firstElementChild);
+	// 输入数字
+	numKeyboard.addEventListener('click', function(e){
+		if (e.target == cancle) {
+			var prevNode = input.lastElementChild.previousElementSibling;
+			input.removeChild(prevNode);
+		}else{
+			if (e.target.className == 'num-item' || e.target.className == 'num-sitem') {
+				var numVal = e.target.firstChild.nodeValue;
+				var numNode = document.createElement('span');
+				numNode.appendChild(document.createTextNode(numVal));
+				input.insertBefore(numNode, input.lastElementChild);
+			}
 		}
+	}, false);
+
+	// 三个绿色按钮的提交
+	var submitLen = submit.length;
+	for (var i = 0; i < submitLen; i++) {
+		submit[i].addEventListener('click', function(e){
+			e.preventDefault();
+			var sumStr = '';
+			var len = input.childNodes.length-2;
+			// console.log(len);
+			for (var i = 0; i < len; i++) {
+				if (input.childNodes[i].nodeType === 1) {
+					sumStr += input.childNodes[i].firstChild.nodeValue;
+				}
+			}
+			console.log(sumStr);
+			sum.value = sumStr;
+		}, false);
 	}
 	
-}, false);
+}
+
